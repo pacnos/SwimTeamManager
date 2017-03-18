8eb4d722f0abcd39a93caa94a776c874905ddcb4
Markus Hinkelmann <markus@mhinkelmann.de>
2017-03-18 23:43:33 +0100

    Finish delete function

--- TeamManager/views/json_views.py
+++ TeamManager/views/json_views.py
@@ -1,4 +1,6 @@
 import json
+
+from django.core.exceptions import ObjectDoesNotExist
 from django.forms import model_to_dict
 from django.http import HttpResponse
 from django.http import HttpResponseBadRequest
@@ -25,7 +27,32 @@ class AthleteContactDetailsJSON(View):
 
         # Load object from database
         athlete_object = Athlete.objects.get(pk=self.args[0])
-        dict = model_to_dict(athlete_object)
-        json_athlete = json.dumps(dict, default=json_helper.json_serial)
+        data_dict = model_to_dict(athlete_object)
+        json_athlete = json.dumps(data_dict, default=json_helper.json_serial)
 
         return HttpResponse(json_athlete, content_type="application/json")
+
+
+class AthleteDeleteJSON(View):
+    """
+    Deletes an Athlete
+    """
+
+    def get(self, request, args=None):
+        """
+        Get Method
+        :param request:
+        :param args:
+        :return:
+        """
+
+        if args is None:
+            return HttpResponseBadRequest()
+
+        try:
+            athlete = Athlete.objects.get(pk=self.args[0])
+            athlete.delete()
+        except ObjectDoesNotExist as ex:
+            return HttpResponseBadRequest("Can't find athlete to delete")
+
+        return HttpResponse()
