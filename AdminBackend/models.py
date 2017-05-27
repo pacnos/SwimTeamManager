f792dd92397b1fe78908b33c7f64c2ff17df3f41
Markus Hinkelmann <markus@mhinkelmann.de>
2017-05-27 23:05:10 +0200

    Translate General Settings and E-Mail Settings

--- AdminBackend/models.py
+++ AdminBackend/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils.translation import ugettext_lazy as _
 
 
 class TMUser(models.Model):
@@ -92,7 +93,7 @@ class GeneralSettings(models.Model):
     SSL = "SSL"
 
     GROUP_CHOICES = (
-        (NO_SECURITY, "None"),
+        (NO_SECURITY, _("None")),
         (TLS, "TLS"),
         (SSL, "SSL")
     )
